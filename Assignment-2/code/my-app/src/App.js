d40ca0debe72a5f4c30989f1f5519241e8a66f56
Zeeshan <Zeeshan1234802@gmail.com>
2022-04-02 22:53:45 +0530

    completed

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,18 +1,46 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
-
 
 function App(props) {
  const {employee}=props;
     
 return(
   <div className="container">
-  {/* Start your React code here */}
+
+  <div className="sub-container">
+
+    <div className="image">
+      <img src={employee.profileImg} alt="" className="profile-pic" />
+      </div>
+
+    <div className="main_name">
+      <p className="name">{employee.name}</p>
+    </div>
+
+    <div className="address">
+      <p>location</p>
+      <h3>{employee.location}</h3>
+    </div>
+
+    <div className="blood">
+      <p>blood group</p>
+      <h3>{employee.bloodGroup}</h3>
+    </div>
+
+    <div className="age">
+      <p>age</p>
+      <h3>{employee.age}</h3>
+    </div>
+
+  </div>
+  
 </div>
 )
 }
 
 
 
+
 export default App;
+
+
